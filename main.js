a8d85ec33ef0be96dacd2ef446adc976d0102086
Jinilp25 <j.psg626@gmail.com>
2024-07-14 23:59:04 -0600

    Changes made

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const square = document.createElement("div");
+const container = document.querySelector("#container");
+
+square.style.height = "50px";
+square.style.width = "50px";
+square.style.backgroundColor = "red";
+
+container.appendChild(square);
