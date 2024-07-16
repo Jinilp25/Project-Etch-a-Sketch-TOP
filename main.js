0ba266f883aa4fde89a3db1726bbc2960c8b88f4
Jinilp25 <j.psg626@gmail.com>
2024-07-15 23:42:40 -0600

    Project Complete Update

--- main.js
+++ main.js
@@ -1,9 +1,62 @@
+function createNewGrid(grid_size) {
+  const container = document.querySelector(".container");
+  square_size = 600 / grid_size;
 
-const square = document.createElement("div");
-const container = document.querySelector("#container");
+  for (let j = 0; j < grid_size; j++) {
+    const row = document.createElement("div");
+    for (let i = 0; i < grid_size; i++) {
+      const square = document.createElement("div");
+      square.style.width = `${square_size}px`;
+      square.style.height = `${square_size}px`;
+      square.classList.toggle("square");
+      row.appendChild(square);
+    }
+    container.appendChild(row);
+  }
+}
 
-square.style.height = "50px";
-square.style.width = "50px";
-square.style.backgroundColor = "red";
+function drawBoard(color) {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((bit) =>
+    bit.addEventListener("mouseover", () => {
+      bit.style.backgroundColor = color;
+    })
+  );
+}
 
-container.appendChild(square);
+function clearBoard() {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((bit) => {
+    bit.style.backgroundColor = "wheat";
+  });
+}
+
+function main() {
+  // default mode
+  const DEFAULTCOLOR = "violet";
+  createNewGrid(16);
+  drawBoard(DEFAULTCOLOR);
+
+  const clearBtn = document.querySelector("#clear");
+  const setSizeBtn = document.querySelector("#setSize");
+  const sizeInput = document.querySelector("input");
+
+  clearBtn.addEventListener("click", () => {
+    clearBoard();
+  });
+
+  setSizeBtn.addEventListener("click", () => {
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+    if (sizeInput.value > 100) {
+      alert("Cannot set value over 100");
+      createNewGrid(16);
+      drawBoard(DEFAULTCOLOR);
+    } else {
+      createNewGrid(sizeInput.value);
+      drawBoard(DEFAULTCOLOR);
+    }
+  });
+}
+
+main();
